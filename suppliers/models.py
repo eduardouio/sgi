aef73cf4982f152ed890d2586d7764ca0cfad551
eduardo <eduardouio7@gmail.com>
2018-12-03 13:40:31 -0500

    Modulo de proveedores

---
+++ suppliers/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+class Supplier(models.Model):
+    identificacion_proveedor = models.CharField(primary_key=True, max_length=16)
+    nombre = models.CharField(unique=True, max_length=60)
+    tipo_provedor = models.CharField(max_length=13, default='NACIONAL')
+    moneda_transaccion = models.CharField(max_length=10)
+    categoria = models.CharField(max_length=250)
+    comentarios = models.CharField(max_length=250, blank=True, null=True)
+    id_user = models.SmallIntegerField(blank=True, null=True, default=0)
+    date_create = models.DateTimeField(blank=True, null=True)
+    last_update = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        managed = False
+        db_table = 'proveedor'
+        verbose_name_plural = 'Proveedores'
+        ordering = ['tipo_provedor', 'nombre']
