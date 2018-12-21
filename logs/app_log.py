d5669487ebc3cfc75669b7c70abf604ed1b4a2e5
eduardo <eduardouio7@gmail.com>
2018-12-21 10:58:16 -0500

    asignacion de usuarios en losgs

--- logs/app_log.py
+++ logs/app_log.py
@@ -2,7 +2,7 @@ import os
 from datetime import datetime 
 path = os.path.dirname(os.path.abspath(__file__)) + '/log.log'
 
-def loggin(type_log , message):
+def loggin(type_log , message, request=None):
     '''
     loggin message in text file
     Args
@@ -15,17 +15,30 @@ def loggin(type_log , message):
         request (django-request): session data for get current user info
     '''
     types_message = {
-        'e':'ERROR',
+        'e':' ERROR ',
         's':'SUCCESS',
         'w':'WARNING',
-        'i':'INFO',
+        'i':' INFO  ',
         }   
+    user_id = 0
+    user_name = ''
+    user_email = ''
+    
+    if request is not None:
+        user_id = request.user.id
+        user_name = request.user
+        user_email = request.user.email
+
     log_file = open(path,'a')
     log_file.write(
-        '[{type_log}] [{date_time}] {message} \n'.format(
-                                            type_log=types_message[type_log],
-                                            date_time=datetime.now(),
-                                            message=message
-                                        )
-        )
+        '[{type_log}]   [{date_time}]   [uid:{user_id} {user_name} {user_email}]    {message} \n'
+            .format(
+                type_log=types_message[type_log],
+                date_time=datetime.now(),
+                message=message,
+                user_id=user_id,
+                user_name = user_name,
+                user_email = user_email,
+            )
+    )
     log_file.close()
