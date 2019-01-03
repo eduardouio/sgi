a1d4da079b6b030ba1665e5c74e7cb7efab9e8bc
eduardo <eduardouio7@gmail.com>
2019-01-03 18:00:23 -0500

    Cambio de motor de plantilas a jinja2

--- sgi/settings.py
+++ sgi/settings.py
@@ -67,13 +67,31 @@ ROOT_URLCONF = 'sgi.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates/'],
+        'DIRS': '',
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+                'sgi.contextprocesor.base_url',
+            ],
+        },
+    },
+    {
+        'BACKEND': 'django.template.backends.jinja2.Jinja2',
+        'DIRS': ['templates/'],
+        'APP_DIRS': False,
+        'OPTIONS': {
+            'environment': 'jinja2_bridge.environment',
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                "django.template.context_processors.i18n",
+                "django.template.context_processors.media",
+                "django.template.context_processors.static",
                 'django.contrib.messages.context_processors.messages',
                 'sgi.contextprocesor.base_url',
             ],
