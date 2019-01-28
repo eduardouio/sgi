dad308a410e3e58ee8dc77956c349ca7fbe98968
eduardo <eduardouio7@gmail.com>
2019-01-28 18:50:38 -0500

    Peticiones ajax movidas a API

--- orders/urls.py
+++ orders/urls.py
@@ -1,11 +1,7 @@
 from django.urls import path, include
-from orders.views.apiViews import GetCompleteOrderInfoAPIView
 from orders.views.OrderSaleTemplateView import OrderSaleTemplateView
-from orders.views.ListOrders import pedidos_lista
 app_name = 'authentication'
 
 urlpatterns = [
-    path('get_all_data/<nro_order>/', GetCompleteOrderInfoAPIView.as_view() ,name="validate_order_json"),
-    path('saldos/', OrderSaleTemplateView.as_view() ,name="sales_of_order"),    
-    path('listado', pedidos_lista ,name="lista"),    
+    path('saldos/', OrderSaleTemplateView.as_view() ,name="sales_of_order"),
 ]
