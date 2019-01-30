336f3cac336ef5b3b064f7b559b4a169f1a69b51
eduardo <eduardouio7@gmail.com>
2019-01-30 18:06:28 -0500

    Fin del dia

--- templates/js/app_partial_costings.js
+++ templates/js/app_partial_costings.js
@@ -2,37 +2,23 @@ var app = new Vue({
     el: '#app',
     delimiters: ['${', '}'],
     data: {
+      ajax_request : false,
       complete_order_info: {},
-      all_partial: {},
+      all_partials: {},
       current_partial : {},
-      init_ledger : 0,      
-      diferecia_mayores : 0.0,
-      ajax_request : true,
-      systems_ledger : 0,
-      current_ledger : {
-        'tipo' : 'inicial',
-        'nro_pedido' : '',
-        'id_parcial' : 0,
-        'costo_producto' : 0,
-        'facturas_sgi' : 0,
-        'mayor_sap' : 0,
-        'mayor_sgi' : 0,
-        'precio_entrega' : 0,
-        'provisiones_sap' : 0,
-        'provisiones_sgi' : 0,
-        'reliquidacion_ice' : 0,
-      },
       csrftoken : Cookies.get('csrftoken'),
     },
     methods : {
       sayHello: function(){
         console.log('Hola Eduardo, Estamos dentro!.')
+      },
+      updateLedger : function(){
+        console.log('Actualizado saldo del Mayor')
       }
     },
     mounted() {
-      this.$http.get('{{BASE_URL}}api/order/all-data/', {params: {}}).then(response => {          
-      this.order_data = response.body 
-      this.ajax_request = false
+      this.$http.get('{{BASE_URL}}api/order/all-data/{{ data.complete_order_info.order.nro_pedido }}', { params: {}}).then(response => {          
+      this.complete_order_info = response.body 
       this.updateLedger()
     }, response => {
       console.log('Se produjo un error, por favor recargue la página');
