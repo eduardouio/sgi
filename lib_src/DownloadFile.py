63dc2abfbf0655ed51b428bfb120657ce88bb3ba
eduardo <eduardouio7@gmail.com>
2019-04-15 21:57:00 -0500

    downloadFile

--- lib_src/DownloadFile.py
+++ lib_src/DownloadFile.py
@@ -1,28 +1,57 @@
 #!/usr/bin/python3
-import requests
-import PyPDF2
 import io
 import urllib
+
+import PyPDF2
+import requests
+import re
+
+
+class DownloadFile(object):
+    
+    def __init__(self, url, type_document):
+        ''' 
+        obtiene unn fichero desde el portal de la senae y obtiene informacion
+        relevante del texto
+    
+        Arguments:
+            type_document {string}
+                tipo de documentop a descargar [DAI | LIQUIDACION]
+        '''
+        self.url = url
+        self.type_document = type_document
+        self.pdf = None
+        self.text_pages = []
+        self.pdf = io.BytesIO(requests.get(url).content)
+        self.data = None
+        pdfReader = PyPDF2.PdfFileReader(self.pdf)
+        #writer = PyPDF2.PdfFileWriter()
+        for p in range(0, pdfReader.numPages):
+            page = pdfReader.getPage(p)
+            self.text_pages.append(page.extractText())
+        
+    def get_text_from_file(self):
+        self.data = {
+            'nro_dai' : None,
+            'fecha_aceptacion' : None,
+            'empresa' : None,
+            'ciudad' : None,
+            'agente' : None,
+            'ruc_agente' : None,
+        }
+        for page in self.text_pages:
+            print(page)
+        
+    
+    def write_pdf(self):
+        pass
+    
+    def get_file(self):
+        pass
+
+
 url = 'http://ecuapass.aduana.gob.ec/ipt_server/cmmReportPdfController.do?method=runPdfReport&fileName=/attach/report/jsp/jasper/report/portal/IptBpIclDclDtlInqrPrn-date-20190403-856E9440-CA19-0EF7-488B-D3BD35841D48.pdf'
+#url = 'http://ecuapass.aduana.gob.ec/ipt_server/cmmReportPdfController.do?method=runPdfReport&fileName=/attach/report/jsp/jasper/report/portal/IptRcTrBillInqrPrn-date-20190403-8566C71D-B30A-570D-C807-49F301973987.pdf'
 
-pdfFile = io.BytesIO(requests.get(url).content)
-#pdfFile = urllib.request.urlopen(url).read()
-writer = PyPDF2.PdfFileWriter()
-#memoryFile = StringIO(pdfFile)
-
-#with open("tutorial1.pdf", "wb") as code:
-#    code.write(pdfFile)
-
-pdfReader = PyPDF2.PdfFileReader(pdfFile)
-for p in range(0,pdfReader.numPages):
-    page = pdfReader.getPage(p)
-    print(page.extractText())
-
-#for pageNum in xrange(pdfFile.getNumPages()):
-#        currentPage = pdfFile.getPage(pageNum)
-#        writer.addPage(currentPage)
-#
-#
-#outputStream = open("output.pdf", "wb")
-#writer.write(outputStream)
-#outputStream.close()
+dw = DownloadFile(url, 'DAI')
+dw.get_text_from_file()
