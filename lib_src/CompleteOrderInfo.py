8b226dca527fe4ac5bf9418c66275ad7f9abc442
eduardo <eduardouio7@gmail.com>
2019-12-11 16:11:05 -0500

    Revision de libreria, se quita la obtencion del ultimo parcial solo se recupera informacion del pedido

--- lib_src/CompleteOrderInfo.py
+++ lib_src/CompleteOrderInfo.py
@@ -10,9 +10,9 @@ from lib_src.serializers import (ApportionmentSerializer, ExpenseSerializer,
                                  RateExpenseSerializer, SupplierSerializer)
 from logs import loggin
 from orders.models import Order, OrderInvoice, OrderInvoiceDetail
-from paids.models import (Expense, PaidInvoice, PaidInvoiceDetail,
-                                  RateExpense)
-from partials.models import Apportionment, Partial
+from paids.models import Expense, PaidInvoice, PaidInvoiceDetail, RateExpense
+from partials.models import (Apportionment, InfoInvoice, InfoInvoiceDetail,
+                             Partial)
 from suppliers.models.Supplier import Supplier
 
 
@@ -61,17 +61,13 @@ class CompleteOrderInfo(object):
             request {http-request} -- django session object (default: {None})
         
         Returns:
-            [type] -- [description]
+            [dict] : Diccionario con toda la informacion del pedido
+                    sin tomar en cuenta los parciales 
         '''
         self.nro_order = nro_order
         self.serialized = serialized
         self.request = request
-        loggin(
-            'i',
-            'Recopilacion de informacion completa del pedido {nro_order}'
-            .format(nro_order = self.nro_order),
-            self.request
-            )
+        loggin('i','Informacion pedido {}'.format(self.nro_order),self.request)
         order  = self.get_order()
 
         if order is None:
@@ -85,9 +81,9 @@ class CompleteOrderInfo(object):
             'taxes' : self.get_taxes(),
             'ledger' : self.get_ledger(),
             'partials' : self.get_partials(),
+            'partials_details' : self.get_partial_details(),
             'status' : self.status_order,
             'tributes' : self.tributes,
-            'files' : self.get_files(),
             'init_ledger' : self.init_ledger,
             'total_expenses' : (self.total_expenses + self.init_ledger),
             'etiquetas_fiscales' : self.etiquetas_fiscales,
@@ -102,6 +98,10 @@ class CompleteOrderInfo(object):
 
     def get_order(self):
         order = Order.get_by_order(nro_order=self.nro_order)
+        if order is None:
+            self.status_order['order'] = False
+            return None
+
         self.tributes = {
             'exoneracion' : 0,
             'arancel_advalorem' : 0,
@@ -110,11 +110,7 @@ class CompleteOrderInfo(object):
             'ice_advalorem' : 0,
             'ice_especifico' : 0,
             'total' : 0,
-        }
-
-        if order is None:
-            self.status_order['order'] = False
-            return None
+            }
 
         self.incoterm = order.incoterm
         
@@ -123,8 +119,8 @@ class CompleteOrderInfo(object):
             self.tributes['arancel_advalorem'] = order.arancel_advalorem_pagar_pagado
             self.tributes['arancel_especifico'] = order.arancel_especifico_pagar_pagado
             self.tributes['fondinfa'] = order.fodinfa_pagado
-            self.tributes['ice_advalorem'] = order.ice_especifico_pagado
-            self.tributes['ice_especifico'] = order.ice_advalorem_pagado
+            self.tributes['ice_especifico'] = order.ice_especifico_pagado
+            self.tributes['ice_advalorem'] = order.ice_advalorem_pagado
 
             self.tributes['total'] = (
                         + (order.arancel_advalorem_pagar_pagado)
@@ -175,13 +171,12 @@ class CompleteOrderInfo(object):
 
         if order_items['order_invoice_details']:
             self.status_order['order_invoice_details'] = True
-
-        for line_item in order_items['order_invoice_details']:
-            order_items['totals']['bottles'] += (line_item.nro_cajas * line_item.cod_contable.cantidad_x_caja)
-            order_items['totals']['boxes'] += line_item.nro_cajas
-            order_items['totals']['value']  += (line_item.nro_cajas * line_item.costo_caja)
-            order_items['totals']['value_tct'] += (line_item.nro_cajas * line_item.costo_caja) * order_invoice.tipo_cambio
-            self.init_ledger  += (line_item.nro_cajas * line_item.costo_caja) * order_invoice.tipo_cambio
+            for line_item in order_items['order_invoice_details']:
+                order_items['totals']['bottles'] += (line_item.nro_cajas * line_item.cod_contable.cantidad_x_caja)
+                order_items['totals']['boxes'] += line_item.nro_cajas
+                order_items['totals']['value']  += (line_item.nro_cajas * line_item.costo_caja)
+                order_items['totals']['value_tct'] += (line_item.nro_cajas * line_item.costo_caja) * order_invoice.tipo_cambio
+                self.init_ledger  += (line_item.nro_cajas * line_item.costo_caja) * order_invoice.tipo_cambio
 
         if self.serialized:
             order_invoice_serializer = OrderInvoiceSerializer(order_items['order_invoice'])
@@ -210,7 +205,6 @@ class CompleteOrderInfo(object):
 
         self.status_order['init_expenses'] = True
         for item in expenses:
-            paids = []
             item.paids = PaidInvoiceDetail.get_by_expense(item)
             item.invoiced_value = 0
             item.ledger = 0
@@ -235,24 +229,26 @@ class CompleteOrderInfo(object):
             if item.concepto == 'ETIQUETAS FISCALES':
                 self.etiquetas_fiscales = item.valor_provisionado
 
+            paids = []
             for paid in item.paids:
                 self.total_invoiced += paid.valor
                 item.invoiced_value += paid.valor
                 paid.invoice = PaidInvoice.get_by_id(paid.id_documento_pago_id)
                 paid.supplier = Supplier.get_by_ruc(paid.invoice.identificacion_proveedor_id)
                 
-                paids.append(
-                    {
-                        'paid' : paid,
-                        'invoice' : paid.invoice,
-                        'supplier': paid.supplier,
-                    }
-                )
+                paids.append({
+                    'paid' : paid,
+                    'invoice' : paid.invoice,
+                    'supplier': paid.supplier,
+                    })
 
                 if paid.bg_mayor == 1:
                     item.ledger += paid.valor
 
             item.sale = (item.valor_provisionado - item.invoiced_value)
+            if item.sale < 0 :
+                loggin('e', 'Gasto {} con saldo negativo'.format(item), self.request)
+
             self.total_provisions += item.sale
 
             if self.serialized:
@@ -294,11 +290,8 @@ class CompleteOrderInfo(object):
         '''
         order = Order.get_by_order(self.nro_order)
         if order is None or order.bg_isclosed == 0:
-            loggin(
-            'w',
-            'No se puede retornar el mayor del pedido {} abierto o inexistente'
-            .format(self.nro_order)
-            )
+            loggin('w','pedido {} abierto o inexistente'.format(self.nro_order),
+            self.request)
             return None
 
         ledger = Ledger().get_by_order(self.nro_order)
@@ -319,24 +312,56 @@ class CompleteOrderInfo(object):
         partials = Partial.get_by_order(self.nro_order)
         if partials.count() == 0:
             return None
-        
-        last_partial = Partial.get_last_close_partial(self.nro_order)
-        
-        if last_partial:
-            last_apportionment = Apportionment.get_by_parcial(last_partial.id_parcial)
-            if self.serialized:
-                apportioment_serializer = ApportionmentSerializer(last_apportionment)
-                self.last_apportionment = apportioment_serializer.data
-            else:
-                self.last_apportionment = last_apportionment
 
-        if self.serialized:
-            partial_serializer = PartialSerializer(partials, many=True)
-            return partial_serializer.data
-   
         return partials
     
 
-    def get_files(self):
-        ''' Obtiene todos los archivos relacionados con el Pedido '''
-        return None
+    def get_partial_details(self):
+        """
+        Retorna informacion consolidad de un parcial, obtiene los datos de la
+        factura informativa y sus totales
+        """
+        partials = Partial.get_by_order(self.nro_order)
+        if partials.count() == 0:
+            return None
+        
+        partials_details = []
+
+        for partial in partials:
+            item_data = {
+                    'id_parcial' : partial.id_parcial,
+                    'nro_refrendo' : None,
+                    'ordial_parcial' : partial.ordinal_parcial,
+                    'fecha_llegada_cliente' : partial.fecha_llegada_cliente,
+                    'fecha_salida_autorizada_almagro' : partial.fecha_salida_autorizada_almagro,
+                    'nro_liquidacion' : partial.nro_liquidacion,
+                    'tipo_cambio' : partial.tipo_cambio,
+                    'bg_isclosed' : partial.bg_isclosed,
+                    'bg_isliquidated' : partial.bg_isliquidated,
+                    'id_factura_informativa' : None,
+                    'nro_factura_informativa' : None,
+                    'moneda' : None,
+                    'nro_cajas' : 0,
+                    'valor_total' : 0,
+                }
+
+            info_invoice = InfoInvoice.get_by_id_partial(partial.id_parcial)
+
+            if info_invoice:
+                item_data['id_factura_informativa'] = info_invoice.id_factura_informativa
+                item_data['nro_factura_informativa'] = info_invoice.nro_factura_informativa
+                item_data['nro_refrendo'] = info_invoice.nro_refrendo
+                item_data['moneda'] = info_invoice.moneda
+            
+            totals_detail = InfoInvoiceDetail.get_totals(partial.id_parcial)
+            
+            if totals_detail:
+                item_data['nro_cajas'] = totals_detail['nro_cajas']
+                item_data['valor_total'] = totals_detail['valor_total']
+            
+            partials_details.append(item_data)
+
+        if self.serialized:
+            return None
+
+        return partials_details if partials_details else None
