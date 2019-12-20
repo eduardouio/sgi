ba6be7ee2778384ea4050f596bc7aa5a809b99d2
eduardo <eduardouio7@gmail.com>
2019-12-19 20:02:21 -0500

    completepaidinfo

--- audit/views/InvoiceTemplateView.py
+++ audit/views/InvoiceTemplateView.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
-from paids.models import PaidInvoice, PaidInvoiceDetail
+from lib_src import CompletePaidInvoice
 
 from lib_src.sgi_utlils import get_host
 
@@ -10,13 +10,12 @@ class InvoiceTemplateView(LoginRequiredMixin,TemplateView):
     template_name = 'audit/mostrar-factura.html'
 
     def get(self, request, id_invoice, *args, **kwargs):
-        #invoice = PaidInvoice.objects.get(id_invoice)
-
         context = self.get_context_data(**kwargs)
         context['data'] = {
             'empresa' : settings.EMPRESA,
             'title_page' : 'Factura {}'.format(id_invoice),
             'host' : get_host(request),
             'request' : request,
-        }
+            'invoice' : CompletePaidInvoice.get(id_invoice)
+        }  
         return self.render_to_response(context)
--- lib_src/CompletePaidInvoice.py
+++ lib_src/CompletePaidInvoice.py
@@ -3,17 +3,12 @@ from orders.models import Order, OrderInvoice, OrderInvoiceDetail
 from paids.models import Expense, PaidInvoice, PaidInvoiceDetail
 from partials.models import InfoInvoice, InfoInvoiceDetail, Partial
 from suppliers.models import Supplier
+from lib_src.sgi_utlils import run_query
 
 
 class CompletePaidInvoice(object):
     """Retorna la informacion completa de una factura"""
 
-    def __init__(self):
-        self.invoice = {
-            'invoice' : None,
-            'details' : None
-        }
-    
     def get(self, id_invoice):
         """Retorna informacion completa de una factura
         
@@ -23,30 +18,86 @@ class CompletePaidInvoice(object):
         Keyword Arguments:
             product_invoice {bool} -- Indica si la factura es de gastos (default: {False})
         """
-        self.invoice['invoice'] = PaidInvoice.get_by_id(id_invoice)
-        if self.invoice['invoice'] is None:
+        loggin('i', 'Informacion completa de Pago')
+        invoice = PaidInvoice.get_by_id(id_invoice)
+        if invoice is None:
             loggin('i', 'La factura que busca no existe')
             return None
         
-        self.invoice['details'] = PaidInvoiceDetail.get_by_paid_invoice(
-            self.invoice['invoice'].id_documento_pago
-        )
-        
         return {
             'invoice' : self.invoice,
-            'order' : self.get_order_data(self.invoice['details']),
-            'parcial' : self.get_parcial_data(),
+            'details' : self.get_details(invoice), 
             'user' : self.get_userdata(),
         }
-    
-    def get_order_data(self, invoice_details):
-        """Obtiene informacion basica de los pedidos"""
-        pass
 
-    def get_parcial_data(self):
-        """Obtiene la informacion completa del parcual"""
-        pass
 
-    def get_user_data(self):
-        """Obtiene la informacion del usuarios"""
-        pass
+    def get_details(self, invoice):
+        """informacion completa detalles factura
+        
+        Arguments:
+            invoice {PaidInvoice} -- Factura
+        """
+        details = PaidInvoiceDetail.get_by_paid_invoice(
+            invoice.id_documento_pago
+            )
+        if details is None:
+            return None
+        
+        details = []
+        for item in details:
+            expense = item.id_gastos_nacionalizacion
+            details.append({
+                'expense' : expense,
+                'detail' : item,
+                'order' : self.get_order_data(expense.nro_pedido),
+                'partial' : self.get_partial_data(expense.id_parcial),
+                'userdata' : self.get_user_data(expense.id_user)
+            })
+        loggin('i', 'Recuperando informacion de factura ')
+        return details
+
+
+    def get_order_data(self, nro_order):
+        """Informacion completa del pedido"""
+        order = Order.get_by_order(nro_order)
+        if order is None:
+            return None
+        order_invoice = OrderInvoice.get_by_order(nro_order)
+        return {
+            'order' : order,
+            'order_invoice' : order_invoice,
+            'supplier' : order_invoice.identificacion_proveedor,
+            'order_details' : OrderInvoiceDetail.get_by_order_invoice(order_invoice.id_pedido_factura)
+        }
+
+
+    def get_partial_data(self, id_partial):
+        """Informacion completa del Parcial"""
+        partial = Partial.get_by_id(id_partial)
+        if partial is None:
+            return None
+        
+        info_invoice = InfoInvoice.get_by_id_partial(partial.id_parcial)
+
+        return {
+            'partial' : partial,
+            'order' : partial.nro_pedido,
+            'info_invoice' : info_invoice,
+            'info_invoice_details' : InfoInvoiceDetail.get_by_info_invoice(info_invoice.id_factura_informativa),
+        }
+
+
+    def get_userdata(self, id_user):
+        """Informacion completa de usuario
+        
+        Arguments:
+            invoice {PaidInvoice} -- Factura
+        """
+        results = run_query(
+            'select * from usuario where id_user = {}'.format(id_user))
+        
+        if results:
+            return results[0]
+        
+        loggin('e', 'No existe el usuario registrado')
+        return None
