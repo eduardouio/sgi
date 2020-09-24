8eaacc88d8c3e2d3f96557369bb89b706baafa15
Eduardo Villota <eduardouio7@gmail.com>
2020-09-24 12:01:23 -0500

    Hola commit de test

--- reports/src/ProvisionsExpenses.py
+++ reports/src/ProvisionsExpenses.py
@@ -1,4 +1,11 @@
+from paids.models import PaidInvoiceDetail, Expense
+
 
 class ProvisionExpenses():
     """ Reporte de provisiones pendientes """
-    pass
+    
+    def get_all():
+        pass
+
+    def check_expenses_with_paids():
+        pass
