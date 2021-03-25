7b0435dfd3699ae3d9eed59fa595fab4ddd8c23f
Eduardo Villota <eduardouio7@gmail.com>
2021-03-25 16:49:55 -0500

    Correccion de reporte ICE, se anade nueva columna

--- reports/views/ICEReportTemplateView.py
+++ reports/views/ICEReportTemplateView.py
@@ -12,41 +12,33 @@ from partials.models import Partial
 class ICEReportTemplateView(LoginRequiredMixin, TemplateView):
     template_name = 'reports/reporte_ice.html'
 
-    def get(self, response,  *args, **kwargs):
+    def get(self, request, *args, **kwargs):
         context = self.get_context_data(**kwargs)
-        report = None
 
-        if response.GET.get('year'):
-            current_year = int(response.GET['year'])
-            current_month = int(response.GET['month'])
-        else:
-            current_year = date.today().year
-            current_month = date.today().month
+        show_error = False
+        report = None
+        current_year = date.today().year
+        current_month = date.today().month
+
+        if request.GET.get('year'):
+            current_year = int(request.GET['year'])
+            current_month = int(request.GET['month'])
 
         reporte_ice = ReportICE(current_year, current_month)
-        query_orders_10 = reporte_ice.get_consumo(query=True)
-        query_partials = reporte_ice.get_partials(query=True)
-        import ipdb;ipdb.set_trace()
-        orders_10 = [o for o in Order.objects.raw(query_orders_10)]
-        partials = [p for p in Partial.objects.raw(query_partials)]
-        show_error = False
-        orders_open = [o for o in orders_10 if o.bg_isclosed != 1]
-        partials_open = [p for p in partials if p.bg_isclosed != 1]
+        report = reporte_ice.get()
 
-        if orders_open or partials_open:
+        if reporte_ice.opened_orders:
             show_error = True
-        else:
-            report = reporte_ice.get()
+
 
         context['data'] = {
             'title_page': 'Rreporte ICE {} {}'.format(
-                current_year, 
+                current_year,
                 calendar.month_name[current_month]),
             'report': report,
             'current_year': current_year,
             'current_month': current_month,
             'show_error': show_error,
-            'partials_open': partials_open,
-            'orders_open': orders_open
+            'orders_opened': reporte_ice.opened_orders,
         }
         return self.render_to_response(context)
