02411f3a2293c7ecc2e56935fb5e5b852f0c73d3
= <=>
2021-09-30 13:17:29 -0500

    Terminamos reporte de proceso activos

--- reports/views/CurrentNationalizationTV.py
+++ reports/views/CurrentNationalizationTV.py
@@ -2,6 +2,8 @@ from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from logs.app_log import loggin
 from django.db import connection
+from datetime import date
+from sgi.settings import EMPRESA
 
 
 class CurrentNationalizationTV(TemplateView, LoginRequiredMixin):
@@ -10,14 +12,25 @@ class CurrentNationalizationTV(TemplateView, LoginRequiredMixin):
     def get(self, request, *args, **kwargs):
         context = self.get_context_data(**kwargs)
         loggin('i', 'Cargando reporte de Productos en Nacionalización')
+        deep = False
+
+        if request.GET:
+            if request.GET.get('deep') == 'on':
+                deep = True
+
         context['data'] = {
             'title_page': 'Reporte En Nacionalización',
-            'report': self.get_report(True)
+            'enterprise': EMPRESA,
+            'report': self.get_report(deep),
+            'today': date.today()
         }
         return self.render_to_response(context)
 
     def get_report(self, show_closed):
-        sql = 'SELECT * FROM `v_current_nationalization` WHERE `bg_isclosed` IS NOT true'
+        sql = (
+            'SELECT * FROM `v_current_nationalization` WHERE'
+            ' `bg_isclosed` IS NOT true'
+        )
         if show_closed:
             sql = 'SELECT * FROM v_current_nationalization'
 
