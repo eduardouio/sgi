e4e9e255ac30e01f9c8a3347a449d1c72b344ec5
Eduardo Villota <eduardouio7@gmail.com>
2024-02-23 15:10:09 -0500

    Añadir funcionalidad de escaneo de datos en SapImportWizardTV.py

--- migrationSAP/views/SapImportWizardTV.py
+++ migrationSAP/views/SapImportWizardTV.py
@@ -1,6 +1,9 @@
 from django.views.generic import TemplateView
 from migrationSAP.lib_src import SAPImporter
 from datetime import date
+from django.conf import settings
+from migrationSAP.lib_src.Model import Model
+
 import json
 
 
@@ -13,6 +16,10 @@ class SapImportWizardTV(TemplateView):
         # sap_importer = SAPImporter()
         # imported = sap_importer.check_orders((date.today()).year)
         imported = []
+        data = None
+        if request.GET.get('scan'):
+            data = Model(settings.EMPRESA['empresa'], (date.today()).year).get_data()
+            data = json.dumps(data, ensure_ascii=False)
         today = date.today()
 
         context['data'] = {
@@ -20,6 +27,7 @@ class SapImportWizardTV(TemplateView):
             'imported': imported,
             'year': today.year,
             'current_status': 'load',
+            'data': data if data else '',
         }
         return self.render_to_response(context)
 
