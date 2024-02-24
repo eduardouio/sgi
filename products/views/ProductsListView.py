fd8a1650104925961f72066475d5c61d7426aacc
Eduardo Villota <eduardouio7@gmail.com>
2024-02-23 23:13:06 -0500

    agregamos estado del producto e indicamos si esta proximo a vencer

--- products/views/ProductsListView.py
+++ products/views/ProductsListView.py
@@ -19,12 +19,33 @@ class ProductListView(LoginRequiredMixin, ListView):
         loggin('i', 'Mostrando lista de productos')
         self.object_list = []
         for item in self.get_queryset():
+            estado = 'Activo' if item.estado == 1 else 'Inactivo'
             if item.fecha_vencimiento_registro:
                 if item.fecha_vencimiento_registro < date.today():
-                    loggin('w', f'Producto {item.nombre} con registro caducado')
-                    item.estado = 0
-            self.object_list.append(item)
+                    loggin(
+                        'w', f'Producto {item.nombre} con registro caducado')
+                    estado = 'Caducado'
 
+                elif item.fecha_vencimiento_registro.year == date.today().year:
+                    if item.fecha_vencimiento_registro.month - date.today().month < 3:
+                        estado = 'Por Caducar'
+
+            self.object_list.append({
+                'cod_contable': item.cod_contable,
+                'nombre': item.nombre,
+                'estado': estado,
+                'identificacion_proveedor': item.identificacion_proveedor.nombre,
+                'cantidad_x_caja': item.cantidad_x_caja,
+                'nro_registro_sanitario': item.nro_registro_sanitario,
+                'fecha_vencimiento_registro': item.fecha_vencimiento_registro,
+                'cod_ice': item.cod_ice,
+
+            })
+
+        order_estado = ['Por Caducar', 'Caducado', 'Activo', 'Inactivo']
+        self.object_list = sorted(
+            self.object_list, key=lambda k: order_estado.index(k['estado'])
+        )
         context = self.get_context_data(**kwargs)
         context['data'] = {
             'title_page': 'Listado Productos'
